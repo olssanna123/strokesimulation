08b6cea0f28748f993f66e51f60dc19e02c4ff15
olssanna123 <57717845+olssanna123@users.noreply.github.com>
2025-08-01 18:07:38 +0200

    Origin package

---
+++ Origin/Origin.py
@@ -0,0 +1,8 @@
+#Origin
+
+# Randomly generated municipality
+# Get borders for municipality as polygon
+# Generate random point within polygon and return coordinates
+# RT90 (swedish grid) coordinates to ?
+# Get GeoJSON municipality feature
+# Flatten ? GeoJSON feature to polygon
