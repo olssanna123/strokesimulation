bff2a35b489a18028dc3cd753e34db50bffaa85c
olssanna123 <57717845+olssanna123@users.noreply.github.com>
2025-08-01 18:12:17 +0200

    Hospital package

---
+++ Hospital/Hospital.py
@@ -0,0 +1,11 @@
+# Hospital
+
+# Find closest emergency hospital
+# Calculate travel time to emergency hospital in sec
+# Calculate travel time from emergency hospital to final hospital in sec
+# Get time at hospital in sec
+# Calculate total time from origin to final hospital via emergency hospital in sec
+# Calculate time from origin to final hospital in sec
+# Decision rules
+# Hospital in adjacent region
+
