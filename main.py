4d6ca5aafe450238903aa7236668b48b91010cd4
olssanna123 <57717845+olssanna123@users.noreply.github.com>
2025-08-24 20:21:55 +0200

    same as gm

--- main.py
+++ main.py
@@ -1,36 +1,11 @@
 # Main
-import matplotlib.pyplot as plt
+from result.database import create_saved_table, create_saved_avg_table
 
-from hospital.hospital import get_time
-from origin.origin import extract_borders, parse_borders, convert_borders
-
-
-def plot_points(points):
-    # Separate the input into x and y lists
-    x_vals = [point[0] for point in points]
-    y_vals = [point[1] for point in points]
-
-    # Plot the points
-    plt.figure(figsize=(8, 6))
-    plt.plot(x_vals, y_vals, marker='o', linestyle='-')
-
-    # Optionally, connect the last point to the first to close the shape
-    plt.plot([x_vals[-1], x_vals[0]], [y_vals[-1], y_vals[0]], marker='o', linestyle='-')
-
-    plt.title('Plot of Points')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.grid(True)
-    plt.show()
 
 def main():
-    borders = extract_borders("Kartogram_SE_filtered", "Härryda")
-    parsed_borders = parse_borders(borders)
-    coords = parsed_borders["Polygon"]
-    tuple_coords = [tuple(coord) for coord in coords]
-    convert_borders(tuple_coords)
-    time = get_time((57.878303, 11.969459), (57.6833, 11.9549))
-    print(str(time) + " s")
+
+    create_saved_table()
+    create_saved_avg_table()
     return
 
 main()
