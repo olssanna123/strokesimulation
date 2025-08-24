27ac57a055969a4f5095039ce524e6cca78960ab
olssanna123 <57717845+olssanna123@users.noreply.github.com>
2025-08-24 20:09:08 +0200

    same as gm

--- hospital/hospital.py
+++ hospital/hospital.py
@@ -1,57 +1,9 @@
 # Hospital
 from data.data import emergency_hospitals, emergency_hospitals_coord
+from google_travel_calc import get_time
 
-# --------------------
-# Helper functions
-def get_time(origin, destination):  # Get the calculated travel time between two destinations
-    pass
 
-def time_difference(a, b):  # Get the time difference
-    if (a > b):
-        return a-b
-    else:
-        return 0
-
-# --------------------
-# Get time at hospital in sec
-def get_time_at_hospital():
-    pass
-
-# --------------------
-# Get travel time
-def get_travel_time(origin, emergency_hospital, final_hospital):
-
-    # Calculate the travel time from origin to the emergency hospital in sec
-    origin_to_emergency_hospital = get_time(origin, emergency_hospital)
-
-    # Time at the emergency hospital in sec
-    time_at_emergency_hospital = get_time_at_hospital()
-
-    # Calculate the travel time from the emergency hospital to the final hospital in sec
-    emergency_hospital_to_final_hospital = get_time(emergency_hospital, final_hospital)
-
-    # Total travel time from origin to final hospital via emergency hospital in sec
-    total_time_via_emergency_hospital = origin_to_emergency_hospital + time_at_emergency_hospital +  emergency_hospital_to_final_hospital
-
-    # Calculate travel time from origin straight to final hospital in sec
-    origin_to_final_hospital = get_time(origin, final_hospital)
-
-    # Calculate the saved time going straight to final hospital in sec
-    saved_time = time_difference(total_time_via_emergency_hospital, origin_to_final_hospital)
-
-    travel_time = {
-        "origin to emergency hospital": origin_to_emergency_hospital,
-        "time at emergency hospital": time_at_emergency_hospital,
-        "emergency hospital to final hospital": emergency_hospital_to_final_hospital,
-        "total time via emergency hospital": total_time_via_emergency_hospital,
-        "origin to final hospital": origin_to_final_hospital,
-        "saved time": saved_time
-    }
-
-    return travel_time
-
-# --------------------
-# Find closest emergency hospital
+# Find the closest emergency hospital
 def get_emergency_hospital(origin):
     closest_emergency_hospital = emergency_hospitals[0][0]
     min_time = get_time(origin, emergency_hospitals[0][1])
@@ -64,12 +16,10 @@ def get_emergency_hospital(origin):
         i = i + 1
     return closest_emergency_hospital
 
-# Name to coordinates of emergency hospital
+# Convert name to coordinates of emergency hospital
 def name_to_coord(name):
     return emergency_hospitals_coord[name]
 
-# --------------------
 # Decision rules
 
-# --------------------
 # Hospital in adjacent region
