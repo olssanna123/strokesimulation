853a0d96bb5dff3ab2d9274a530ccfbed073d42f
olssanna123 <57717845+olssanna123@users.noreply.github.com>
2025-08-24 20:18:54 +0200

    same as gm

--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -1,7 +1,31 @@
-# Monte Carlo
-from origin.origin import draw_sample
+from data.data import get_borders
+from google_travel_demo import get_travel_time_demo
+from hospital.hospital import get_emergency_hospital
+from origin.origin import draw_sample, get_origin
+from result.database import write_saved
 
-def monte_carlo(sampling_array):
+
+def loop_demo(sampling_array, final_hospital):
     mun = draw_sample(sampling_array)
-    return mun
+    borders = get_borders(mun)
+    origin = get_origin(borders)
+    emergency_hospital = get_emergency_hospital(origin)
 
+    time = get_travel_time_demo(origin, emergency_hospital, final_hospital)
+
+    if emergency_hospital == "Sahlgrenska Universitetssjukhuset":
+        print("Sahlgrenska Universitetssjukhuset")
+        saved_time = 0
+        write_saved(saved_time)
+    else:
+        saved_time = time["Origin to emergency hospital"]["total"]  + 7200 + time["Emergency hospital to final hospital"]["total"] - time["Origin to final hospital"]["total"]
+        print("Saved time")
+        print(saved_time)
+        write_saved(saved_time)
+
+    res = {
+        "Origin": origin,
+        "Emergency hospital": emergency_hospital,
+    }
+
+    return res
