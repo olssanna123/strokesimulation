e3dfeec1891cacf0135cb7666687edcd2670c7ac
olssanna123 <57717845+olssanna123@users.noreply.github.com>
2025-08-25 20:08:52 +0200

    demo front end

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # Main
+from data.data import create_sampling_array
 
 
 def main():
+    array = create_sampling_array()
+
     return
 
 main()
--- montecarlo/loop.py
+++ montecarlo/loop.py
@@ -1,7 +1,7 @@
-from data.data import get_borders, emergency_hospitals, get_hospitals
+from data.data import get_borders, get_hospitals
 from hospital.hospital import name_to_coord, get_emergency_hospital
 from origin.origin import draw_sample, get_origin
-from osrm_travel import get_time, to_osrm
+from osrm_travel import get_time
 
 
 def loop(sampling_array):
@@ -15,25 +15,9 @@ def loop(sampling_array):
         print("Sahlgrenska Universitetssjukhuset is the closest emergency hospital!")
         saved_time = 0
         print("Saved time is " + str(saved_time) + " seconds")
-#        write_saved(saved_time)
-    else:
-        print("Time from origin to emergency hospital is: ")
-        time1 = emergency_hospital[1]
-        print(str(time1) + " seconds")
-
-        print("Time from emergency hospital to Sahlgrenska is: ")
-        time2 = get_time(to_osrm(name_to_coord(emergency_hospital[0])), to_osrm(name_to_coord("Sahlgrenska Universitetssjukhuset")))
-        print(str(time2) + " seconds")
-
-        print("Time straight from origin to Sahlgrenska is: ")
-        time3 = get_time(to_osrm(origin), to_osrm(name_to_coord("Sahlgrenska Universitetssjukhuset")))
-        print(str(time3) + " seconds")
-
-        #write_saved(saved_time)
-
     res = {
         "Origin": origin,
-        "Emergency hospital": emergency_hospital[0],
+        "Emergency hospital": emergency_hospital[0]
     }
 
     print(res)
