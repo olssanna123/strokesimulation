b50c7d29f06ab09f5d846d239ecfa8c42eb9b4e5
olssanna123 <57717845+olssanna123@users.noreply.github.com>
2025-08-25 11:51:37 +0200

    demo front end

--- hospital/hospital.py
+++ hospital/hospital.py
@@ -1,5 +1,5 @@
 from data.data import emergency_hospitals_coord
-from osrm_travel import get_time, to_osrm
+from osrm_travel import get_time, format_coordinates
 
 SAHLGRENSKA = "Sahlgrenska Universitetssjukhuset"
 
@@ -24,15 +24,8 @@ def get_emergency_hospital(origin, hospitals):
         return hospitals  # return whatever is available
 
     # Create a list of (hospital, travel_time) tuples
-    times = [(hospital, get_time(to_osrm(origin), to_osrm(name_to_coord(hospital)))) for hospital in hospitals]
+    times = [(hospital, get_time(origin, name_to_coord(hospital))) for hospital in hospitals]
 
     # Sort by travel time
     times.sort(key=lambda x: x[1])
-
-    time_to_sahl1 = times[0][1] + get_time(to_osrm(name_to_coord(times[0][0])), to_osrm(name_to_coord(SAHLGRENSKA)))
-    time_to_sahl2 = times[1][1] + get_time(to_osrm(name_to_coord(times[1][0])), to_osrm(name_to_coord(SAHLGRENSKA)))
-
-    if time_to_sahl2 < time_to_sahl1 and times[1][1] < 900:
-        print(times[0])
-    else:
-        print(times[1])
+    print(times)
--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from hospital.hospital import get_emergency_hospital
 
 
 def main():
-    varberg = (57.10557, 12.25078)
-    get_emergency_hospital(varberg, get_hospitals())
+    varberg = '57.10557, 12.25078'
+    hospitals = get_hospitals()
+    get_emergency_hospital(varberg, hospitals)
     return
 
 main()
