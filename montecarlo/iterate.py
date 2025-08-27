1644cf213079db016b83131cffbed5e9ef59583a
olssanna123 <57717845+olssanna123@users.noreply.github.com>
2025-08-27 14:37:04 +0200

    demo front end

--- montecarlo/iterate.py
+++ montecarlo/iterate.py
@@ -28,12 +28,12 @@ def loopOS(sampling_array):
         saved_time = 0
         print("Saved time is " + str(saved_time) + " seconds")
         #write_saved(saved_time)
-        return res
     else:
         print("The closest emergency hospital is " + emergency_hospital[0])
-        via_emergency_hospital = get_time(origin_formatted, emergency_hospital) + 7200 # 2 h at emergency hospital
-        straight_to_sahlgrenska = get_time(origin_formatted, name_to_coord("Sahlgrenska Universitetssjukhuset"))
-        saved_time = via_emergency_hospital - straight_to_sahlgrenska
-        print("Saved time is " + str(saved_time))
+#        via_emergency_hospital = get_time(origin_formatted, emergency_hospital) + 7200 # 2 h at emergency hospital
+#        straight_to_sahlgrenska = get_time(origin_formatted, name_to_coord("Sahlgrenska Universitetssjukhuset"))
+#        saved_time = via_emergency_hospital - straight_to_sahlgrenska
+#        print("Saved time is " + str(saved_time))
         # write_saved(saved_time)
-        return res
+
+    return res
