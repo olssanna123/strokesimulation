6fcef6f412361a581e6d1f16bac2bba0357fd924
olssanna123 <57717845+olssanna123@users.noreply.github.com>
2025-08-27 10:39:31 +0200

    demo front end

--- montecarlo/loop.py
+++ montecarlo/loop.py
@@ -1,6 +1,8 @@
 from data.data import get_borders, get_hospitals
 from hospital.hospital import name_to_coord, get_emergency_hospital
+from main import convert_seconds
 from origin.origin import draw_sample, get_origin
+from osrm_travel import get_time
 from result.result import write_saved
 
 
@@ -8,8 +10,18 @@ def loop(sampling_array):
     mun = draw_sample(sampling_array)
     borders = get_borders(mun)
     origin = get_origin(borders)
+
     hospitals = get_hospitals()
     emergency_hospital = get_emergency_hospital(origin, hospitals)
+
+    # Origin
+    print(origin)
+    origin_formatted = (origin[1],origin[0])
+    print(origin_formatted)
+    time = get_time(origin_formatted, name_to_coord("Sahlgrenska Universitetssjukhuset"))
+    print(convert_seconds(time))
+
+
 
     res = {
         "Origin": origin,
