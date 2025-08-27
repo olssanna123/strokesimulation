261b2bcebaf1df2d04cadb146c042d7fd5942049
olssanna123 <57717845+olssanna123@users.noreply.github.com>
2025-08-27 15:01:31 +0200

    demo front end

--- just_small_tests.py
+++ just_small_tests.py
@@ -5,13 +5,17 @@ from origin.origin import draw_sample, get_origin
 from osrm_travel import get_time
 
 x = create_sampling_array()
-mun = draw_sample(x)
-print(mun)
-borders = get_borders(mun)
-origin = get_origin(borders)
-print(origin)
-origin_formatted = (origin[1], origin[0])
-print(origin_formatted)
-time = get_time(origin_formatted, name_to_coord("Sahlgrenska Universitetssjukhuset"))
-print(convert_seconds(time))
+
+i = 0
+while i < 30:
+    mun = draw_sample(x)
+    print(mun)
+    borders = get_borders(mun)
+    origin = get_origin(borders)
+    print(origin)
+    origin_formatted = (origin[1], origin[0])
+    print(origin_formatted)
+    time = get_time(origin_formatted, name_to_coord("Sahlgrenska Universitetssjukhuset"))
+    print(convert_seconds(time))
+    i += 1
 
